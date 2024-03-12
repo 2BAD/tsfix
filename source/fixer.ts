3b648efb10396a60da17195ac6d73f06d6adb5b5
Jason Hyde <github@2bad.me>
2024-03-12 13:37:23 +0100

    [ts] run replacer only once per import

--- source/fixer.ts
+++ source/fixer.ts
@@ -8,10 +8,9 @@ import { type ParsedImport } from './types.ts'
  */
 export const fixImport = (code: string, imports: ParsedImport[]): string => {
   for (const i of imports) {
-    if (i.type === 'relative') {
-      code = i.extension === '.ts' ? code.replace(i.specifier, i.specifier.substring(0, i.specifier.length - 3)) : code
-      i.specifier = i.specifier.replace(i.extension ?? '.ts', '')
-      code = code.replace(i.specifier, i.specifier + '.js')
+    console.log(i.specifier)
+    if ((i.type === 'absolute' || i.type === 'relative') && i.extension === '.ts') {
+      code = code.replace(i.specifier, i.specifier.replace('.ts', '.js'))
     }
   }
   return code
