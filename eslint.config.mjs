77902be7ec6caf186f53f6bd1a3fd0cbeca4df2a
Jason Hyde <github@2bad.me>
2025-03-05 02:06:24 +0100

    [misc] disable conflicting eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,13 @@ import { axiom } from '@2bad/axiom'
 
 // biome-ignore lint/style/noDefaultExport: acceptable for this use case
 export default [
-  axiom(import.meta.dirname),
   {
     ignores: ['bin']
+  },
+  axiom(import.meta.dirname),
+  {
+    rules: {
+      'import-x/no-named-as-default-member': 'off'
+    }
   }
 ].flat()
